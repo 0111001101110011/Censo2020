3e19204f6bc09a7b99c86f1f798c614be51c5954
0111001101110011 <gapm55@gmail.com>
2020-04-06 16:25:07 -0700

    gets casas y negocios

---
+++ app/Http/Controllers/CasasApiController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Casa;
+
+class CasasApiController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //Cada respuesta regresa 20 casas
+        
+        //Solicito información
+        $paginaActual = intval($request->input('pagina'));
+        if(!$paginaActual)  {
+            $paginaActual = 1;
+        }
+
+
+        $numeroCasas = Casa::count();  
+        $numeroPaginas = ceil($numeroCasas/20);
+        $casas = Casa::where('id_user',$request->user()->id)-> 
+            
+        skip(($paginaActual - 1) * 20)->take(20)->get();
+        
+        //Construyo respuesta
+        $respuesta =  array();
+        $respuesta['total'] = $numeroCasas; 
+        $respuesta['paginas'] = $numeroPaginas;
+        $respuesta['pagina_actual'] = $paginaActual;
+        $respuesta['casas'] = $casas;
+        
+        //Envio respuesta
+        return $respuesta;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $nuevaCasa = new Casa();
+        $nuevaCasa->id_User  = $request->user()->id;
+        $nuevaCasa->calle = $request->input('calle');
+        $nuevaCasa->numero = $request->input('numero');
+        $nuevaCasa->numero_interior =
+            $request->input('numero_interior');
+        $nuevaCasa->colonia = $request->input('colonia');
+        $nuevaCasa->numero_banos =
+            $request->input('numero_banos');
+        $nuevaCasa->numero_habitantes =
+            $request->input('numero_habitantes');
+        
+            //Armar respuesta
+            $respuesta = array();
+            $respuesta['exito'] = false;
+            if ($nuevaCasa->save()) {
+                $respuesta['exito'] = true;
+            }
+
+            return $respuesta;
+        } 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/NegociosApiController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Negocio;
+
+class NegociosApiController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(Request $request)
+    {
+        $negocios = Negocio::where('id_user',$request->user()->id)-> get();
+        return $negocios;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $nuevoNegocio = new Negocio();
+        $nuevoNegocio->id_User  = $request->user()->id;
+        $nuevoNegocio->calle = $request->input('calle');
+        $nuevoNegocio->numero = $request->input('numero');
+        $nuevoNegocio->numero_interior =$request->input('numero_interior');
+        $nuevoNegocio->colonia = $request->input('colonia');
+        $nuevoNegocio->nombre_negocio = $request->input('nombre_negocio');
+        $nuevoNegocio->numero_empleados = $request->input('numero_empleados');
+        $nuevoNegocio->numero_banos = $request->input('numero_banos');
+        
+        
+            //Armar respuesta
+            $respuesta = array();
+            $respuesta['exito'] = false;
+            if ($nuevoNegocio->save()) {
+                $respuesta['exito'] = true;
+            }
+
+            return $respuesta;
+        } 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
